9ef0c8ce711205678599ac3c79e9334ceaa547fa
Adrian Pappalardo <adrian.pappalardo@gmail.com>
2023-01-30 14:30:25 +0100

    full body avatar added

--- src/views/components/Avatar/Avatar.style.ts
+++ src/views/components/Avatar/Avatar.style.ts
@@ -1,18 +1,27 @@
 import styled from 'styled-components'
 
 export const AvatarContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
 `
 
 export const AvatarName = styled('div')`
-  margin-top: -30px;
+	margin-top: -30px;
 `
 
 export const AvatarLineArt = styled('div')`
-  margin-right: -70px;
-  margin-bottom: -219px;
+	margin-right: -70px;
+	margin-bottom: -700px;
+`
+
+interface AvatarViewInterface {
+	show: boolean
+}
+export const AvatarView = styled('div')<AvatarViewInterface>`
+	display: ${({ show }) => (show ? 'block' : 'none')};
+	height: 45vh;
+	margin-top: 30vh;
 `
--- src/views/components/Avatar/Avatar.tsx
+++ src/views/components/Avatar/Avatar.tsx
@@ -1,22 +1,41 @@
+import { Suspense, useRef, useState } from 'react'
 import { AvatarTitle } from '../../../style/fonts.style'
 import GlassCard from '../GlassCard/GlassCard'
-import { AvatarContainer, AvatarLineArt, AvatarName } from './Avatar.style'
+import { AvatarContainer, AvatarLineArt, AvatarName, AvatarView } from './Avatar.style'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Model } from './Model'
 
 const Avatar = () => {
-  return (
-    <AvatarContainer>
-      <AvatarLineArt>
-        <img src='line-art.svg' width={300} alt='line-art' />
-      </AvatarLineArt>
-      <img src='profile.png' width={200} alt='Profile' />
-      <AvatarName>
-        <GlassCard>
-          <img src='logo.svg' width={30} alt='logo' />
-          <AvatarTitle>Adrian Pappalardo</AvatarTitle>
-        </GlassCard>
-      </AvatarName>
-    </AvatarContainer>
-  )
+	const avatarRef = useRef(null)
+	const bodyRef = useRef(null)
+	const [toggleBody, setBody] = useState(true)
+
+	return (
+		<AvatarContainer>
+			<AvatarLineArt>
+				<img src='line-art.svg' width={300} alt='line-art' />
+			</AvatarLineArt>
+			<AvatarView ref={bodyRef} show={toggleBody}>
+				<Canvas camera={{ position: [2, 0, 12.25], fov: 15 }}>
+					<ambientLight intensity={1.25} />
+					<ambientLight intensity={0.1} />
+					<directionalLight intensity={0.4} />
+					<Suspense fallback={null}>
+						<Model position={[1, -1.3, 6]} />
+					</Suspense>
+				</Canvas>
+			</AvatarView>
+			<AvatarView ref={avatarRef} show={!toggleBody}></AvatarView>
+			{/* <img src='profile.png' width={200} alt='Profile' /> */}
+			<AvatarName>
+				<GlassCard>
+					<img src='logo.svg' width={30} alt='logo' />
+					<AvatarTitle>Adrian Pappalardo</AvatarTitle>
+				</GlassCard>
+			</AvatarName>
+		</AvatarContainer>
+	)
 }
 
 export default Avatar
